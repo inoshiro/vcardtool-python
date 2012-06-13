4d9a4a08179b2bee0343dbde27ca8f0aa66ecee7
Kenjiro Kosaka <inoshirou@gmail.com>
2012-06-13 10:41:51 +0900

    add parameter for setup()

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 setup(name='vcardtool',
       version='0.0.1',
+      author='Kenjiro Kosaka',
+      author_email='inoshirou@gmail.com',
+      url='https://github.com/inoshiro/vcardtool-python',
       py_modules=['vcardtool'],
       install_requires=['vobject'])
