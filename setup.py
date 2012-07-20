d496cc5c59bc53ecc90fa0a8953ede1ee1823ade
Kenjiro Kosaka <kenjiro@ubuntu-2nd.(none)>
2012-07-20 16:21:24 +0900

    script as console_scripts

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+entry_points = {
+    'console_scripts':[
+        'vcardtool = vcardtool:main'
+    ]
+}
+
 setup(name='vcardtool',
       version='0.0.1',
       author='Kenjiro Kosaka',
       author_email='inoshirou@gmail.com',
       url='https://github.com/inoshiro/vcardtool-python',
       py_modules=['vcardtool'],
-      install_requires=['vobject'])
+      install_requires=['vobject'],
+      entry_points=entry_points)
